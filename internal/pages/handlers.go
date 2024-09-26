8d0eb7a7bc96e011c4145629f420f9b1f9a5fa1a
JeffMuter <muterjeffery@gmail.com>
2024-09-26 10:28:09 -0400

    added homepage route

--- internal/pages/handlers.go
+++ internal/pages/handlers.go
@@ -1,1 +1,13 @@
 package pages
+
+import (
+	"net/http"
+)
+
+func ServeHomePage(w http.ResponseWriter, r *http.Request) { // show login form page
+	data := TemplateData{
+		Data: map[string]string{
+			"Title": "Home",
+		}}
+	RenderLayoutTemplate(w, "homePage", data)
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 	"pact/internal/auth"
+	"pact/internal/pages"
 	"pact/internal/stripe"
 )
 
@@ -13,11 +14,11 @@ func Router() *http.ServeMux {
 	// Register handlers
 	//	mux.Handle("/", auth.AuthMiddleware(http.HandlerFunc(pages.ServeHomePage)))
 
-	mux.HandleFunc("GET /login", auth.ServeLoginPage)
+	mux.HandleFunc("GET /loginPage", auth.ServeLoginPage)
 	mux.HandleFunc("GET /loginForm", auth.ServeLoginForm)
 	mux.HandleFunc("POST /login", auth.LoginFormHandler)
 
-	mux.HandleFunc("GET /register", auth.ServeRegistrationPage)
+	mux.HandleFunc("GET /registerPage", auth.ServeRegistrationPage)
 	mux.HandleFunc("GET /registerForm", auth.ServeRegistrationForm)
 	mux.HandleFunc("POST /registeruser", auth.RegisterHandler)
 
@@ -27,8 +28,10 @@ func Router() *http.ServeMux {
 
 	// Serve static files
 	fileServer := http.FileServer(http.Dir("static"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	mux.Handle("/js/", http.StripPrefix("/js/", fileServer))
-	mux.Handle("/images/", http.StripPrefix("/images/", fileServer))
+	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
+	mux.Handle("GET /js/", http.StripPrefix("/js/", fileServer))
+	mux.Handle("GET /images/", http.StripPrefix("/images/", fileServer))
+
+	mux.HandleFunc("GET /", pages.ServeHomePage)
 	return mux
 }
